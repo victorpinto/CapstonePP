ce70b05b83ca429f004a4339893f3e4849b319a1
Andre Giroux <andre_e_giroux@hotmial.com>
2018-07-24 14:34:33 -0400

    Camera Updated, controller updated
    Camera has been fixed to the player, before the camera would lag behind. The camera has been given a 180 degree rotation. Added Kevins player controller.

---
+++ Assets/Scripts/PlayerCamera180LockScript.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayerCamera180LockScript : MonoBehaviour {
+
+    
+    private Cinemachine.CinemachineFreeLook playerVirtualCamera;
+
+    // From Cinemachine Dev
+    /*
+    [Range(0, 360)]
+    private float deadZoneSize = 180;
+    */
+ 
+
+	// Use this for initialization
+	void Start () {
+        playerVirtualCamera = transform.GetComponent<Cinemachine.CinemachineFreeLook>();
+
+
+        
+    }
+	
+
+    
+
+	// Update is called once per frame
+	void Update () {
+
+        //CinemachineCore.GetInputAxis().;
+        // left = 90
+        // right = 270
+
+        //90<->180<->270 == NO!!
+
+        /////////////////////////////////////////////////////
+
+        // From Cinemachine Dev
+        /*
+        float halfDeadZone = deadZoneSize / 2;
+        float axisXValue = playerVirtualCamera.m_XAxis.Value;
+
+        if (axisXValue > 180)
+        {
+            axisXValue = Mathf.Max(axisXValue, 180 + halfDeadZone);
+        }
+        if (axisXValue < 180)
+        {
+            axisXValue = Mathf.Min(axisXValue, 180 - halfDeadZone);
+        }
+
+        playerVirtualCamera.m_XAxis.Value = axisXValue;
+        */
+
+        //////////////////////////////////////////////////////////////////
+        
+        if (playerVirtualCamera.m_XAxis.Value >= 90 && playerVirtualCamera.m_XAxis.Value <= 270)
+        {
+            if (playerVirtualCamera.m_XAxis.Value >= 90 && playerVirtualCamera.m_XAxis.Value < 181)
+            {
+
+                //90
+                playerVirtualCamera.m_XAxis.Value = 90.0f;
+                //playerVirtualCamera.m_XAxis.m_AccelTime = 0;
+                
+                //Debug.Log("Entered1");
+            }
+
+
+            else if (playerVirtualCamera.m_XAxis.Value <= 270 && playerVirtualCamera.m_XAxis.Value > 179)
+            {
+                // 270
+                playerVirtualCamera.m_XAxis.Value = 270.0f;
+               // Debug.Log("Entered2");
+            } 
+        }
+        
+        
+        ////////////////////////////////////////////////////////
+
+        //playerVirtualCamera.m_XAxis.Value = Mathf.Clamp(playerVirtualCamera.m_XAxis.Value, 90, 270);
+
+    }
+
+    /*
+    public float GetAxisCustom(string axisName)
+    {
+        Debug.Log("FART");
+        if (axisName == "Horizontal")
+        {
+            return -UnityEngine.Input.GetAxis("Horizontal");
+        }
+        else
+            return UnityEngine.Input.GetAxis(axisName);
+    }
+    */
+    /*
+    private float GetAxisCustom(string axisName)
+    {
+        Debug.Log("Hoi!");
+        if (axisName == "RightStickX")
+        {
+            Debug.Log("it has found right!");
+            if (playerVirtualCamera.m_XAxis.Value >= 90 && playerVirtualCamera.m_XAxis.Value <= 270)
+            {
+                Debug.Log("It was discoverd");
+                return 0;   
+            }
+            else
+                return Input.GetAxis("RightStickX");
+        }
+        else
+            return 0;
+    }
+
+    */
+
+    //Deffault Axis controls (X axis -> Input Axis Name = Mouse X, Y axis -> Input Axis Name = Mouse Y)
+
+}
--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -3,38 +3,172 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+//[RequireComponent(typeof(ConfigurableJoint))]
 public class ThirdPersonController : MonoBehaviour {
 
-    // Serialized Fields
-    // Player Attributes
+    // Serielized
+    // Movement
     [SerializeField]
-    private float Acceleration = 2;
+    private float AccerationForce = 100000;
     [SerializeField]
-    private float backwardMultiplayer = 0.5f;
+    private float MaxVelocity = 20;
     [SerializeField]
-    private float sprintMultiplayer = 2;
+    private float TurningForce = 10000;
     [SerializeField]
-    private float springInitialLength = 2;
+    private float MaxAngularVelocity = 180;
     [SerializeField]
-    private float Mass = 10;
+    private float JumpForce = 250;
+    [SerializeField]
+    private float SpeedSlow = 80;
+    [SerializeField]
+    private float TurnSlow = 200;
 
-    // Objects
+    // Character Attributes
     [SerializeField]
-    private Camera playerCamera;
+    private float PlayerMass = 80;
+    [SerializeField]
+    private float FloatDistance = 0.2f;
+    [SerializeField]
+    private LayerMask GroundLayer;
+    [SerializeField]
+    private Transform Feet;
 
-    // non - Serialized Fields
+    // Not Serielized
+    // Player components
+    private Rigidbody RB;
+    private Transform Trans;
+    //private ConfigurableJoint CJ;
+
+    // Active attributes
+    private float Speed = 0;
+    private float AngularSpeed = 0;
+    private float Acceleration = 0;
+    private float TurningAcceleration = 0;
+    private bool onGround = true;
 
 
-    // Use this for initialization
+    // Recieved attributes
+    private float MoveAxis = 0;
+    private float TurnAxis = 0;
+    private float JumpAxis = 0;
+
     void Start () {
-		
+
+        RB = GetComponent<Rigidbody>();
+        RB.mass = PlayerMass;
+        //CJ = GetComponent<ConfigurableJoint>();
+        //CJ.connectedAnchor = new Vector3(0, FloatDistance, 0);
+        Trans = transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 bodyRotation = new Vector3(0, playerCamera.transform.eulerAngles.y, 0);
 
-        transform.rotation = Quaternion.Euler(bodyRotation);
+        // Get Axis of Movement
+        MoveAxis = Input.GetAxis("Move");
+        TurnAxis = Input.GetAxis("Turn");
 
-	}
+        //Debug.Log("Move Axis: " + MoveAxis);
+        //Debug.Log("Turn Axis: " + TurnAxis);
+        // Accelerate/ Deccelerate
+        // Speed
+        if (MoveAxis > 0.1f || MoveAxis < -0.1)
+        {
+            if (Mathf.Abs(Speed) <= MaxVelocity)
+            {
+                Acceleration = MoveAxis * AccerationForce * Time.deltaTime / PlayerMass;
+            }
+            else
+            {
+                Acceleration = 0;
+            }
+        }
+        else
+        {
+            Acceleration = 0;
+            if (Mathf.Abs(Speed) > 5f)
+            {
+                Speed -= Speed * SpeedSlow * Time.deltaTime / 100;
+            }
+            else if (Mathf.Abs(Speed) > 1f)
+            {
+                Speed -= Speed * 95 * Time.deltaTime / 100;
+            }
+            else if (Mathf.Abs(Speed) > 0.1f)
+            {
+                Speed -= Speed * 99 * Time.deltaTime / 100;
+            }
+            else
+                Speed = 0;
+        }
+
+        // Turning
+        if (TurnAxis > 0.1f || TurnAxis < -0.1)
+        {
+            if (Mathf.Abs(AngularSpeed) <= MaxAngularVelocity)
+            {
+                TurningAcceleration = TurnAxis * TurningForce * Time.deltaTime * 180 / PlayerMass;
+            }
+            else
+            {
+                TurningAcceleration = 0;
+            }
+        }
+        else
+        {
+            TurningAcceleration = 0;
+            if (Mathf.Abs(AngularSpeed) > 10)
+            {
+                AngularSpeed -= AngularSpeed * TurnSlow * Time.deltaTime / 100;
+            }
+            else
+            {
+                AngularSpeed = 0;
+            }
+        }
+
+        // Move Player
+        Speed += Acceleration * Time.deltaTime;
+        if (Speed > MaxVelocity)
+        {
+            Speed = MaxVelocity;
+        }
+        else if (Speed < -MaxVelocity)
+        {
+            Speed = -MaxVelocity;
+        }
+        //Debug.Log("Speed: " + Speed + " Acceleration: " + Acceleration);
+        Trans.position += Trans.forward * Speed * Time.deltaTime;
+
+        // Turn Player
+        AngularSpeed += TurningAcceleration;
+        if (AngularSpeed > MaxAngularVelocity)
+        {
+            AngularSpeed = MaxAngularVelocity;
+        }
+        else if (AngularSpeed < -MaxAngularVelocity)
+        {
+            AngularSpeed = -MaxAngularVelocity;
+        }
+        //Debug.Log("Angular Speed: " + AngularSpeed + " Angular Acceleration: " + TurningAcceleration);
+        Trans.Rotate(0, AngularSpeed * Time.deltaTime, 0);
+
+        // Jump
+        JumpAxis = Input.GetAxis("Jump");
+        onGround = Physics.Raycast(Feet.position, Vector3.down, FloatDistance + 0.3f, GroundLayer);
+        //Debug.Log("Jump Axis: " + JumpAxis);
+        if (onGround)
+        {
+            Debug.Log("ON GROUND");
+        }
+        else
+        {
+            Debug.Log("NOT ON GROUND");
+        }
+
+        if (JumpAxis > 0.1f && onGround)   
+        {
+            RB.AddForce(Trans.up * JumpForce, ForceMode.Impulse);
+        }
+    }
 }
