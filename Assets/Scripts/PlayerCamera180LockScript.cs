7696bf6e37fcb2edc47511e37ebec84c7c3cb2ea
Andre Giroux <andre_e_giroux@hotmial.com>
2018-07-25 16:28:48 -0400

    Crosshair cross
    Added camera max rotation notification

--- Assets/Scripts/PlayerCamera180LockScript.cs
+++ Assets/Scripts/PlayerCamera180LockScript.cs
@@ -3,59 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
-public class PlayerCamera180LockScript : MonoBehaviour {
+public class PlayerCamera180LockScript : MonoBehaviour
+{
 
-    
+
     private Cinemachine.CinemachineFreeLook playerVirtualCamera;
 
-    // From Cinemachine Dev
-    /*
-    [Range(0, 360)]
-    private float deadZoneSize = 180;
-    */
- 
+  
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         playerVirtualCamera = transform.GetComponent<Cinemachine.CinemachineFreeLook>();
 
 
-        
+
     }
-	
 
-    
 
-	// Update is called once per frame
-	void Update () {
 
-        //CinemachineCore.GetInputAxis().;
-        // left = 90
-        // right = 270
 
-        //90<->180<->270 == NO!!
+    // Update is called once per frame
+    void Update()
+    {
+        //if ((player))
 
-        /////////////////////////////////////////////////////
-
-        // From Cinemachine Dev
-        /*
-        float halfDeadZone = deadZoneSize / 2;
-        float axisXValue = playerVirtualCamera.m_XAxis.Value;
-
-        if (axisXValue > 180)
-        {
-            axisXValue = Mathf.Max(axisXValue, 180 + halfDeadZone);
-        }
-        if (axisXValue < 180)
-        {
-            axisXValue = Mathf.Min(axisXValue, 180 - halfDeadZone);
-        }
-
-        playerVirtualCamera.m_XAxis.Value = axisXValue;
-        */
-
-        //////////////////////////////////////////////////////////////////
-        
         if (playerVirtualCamera.m_XAxis.Value >= 90 && playerVirtualCamera.m_XAxis.Value <= 270)
         {
             if (playerVirtualCamera.m_XAxis.Value >= 90 && playerVirtualCamera.m_XAxis.Value < 181)
@@ -63,9 +35,7 @@ public class PlayerCamera180LockScript : MonoBehaviour {
 
                 //90
                 playerVirtualCamera.m_XAxis.Value = 90.0f;
-                //playerVirtualCamera.m_XAxis.m_AccelTime = 0;
                 
-                //Debug.Log("Entered1");
             }
 
 
@@ -73,50 +43,13 @@ public class PlayerCamera180LockScript : MonoBehaviour {
             {
                 // 270
                 playerVirtualCamera.m_XAxis.Value = 270.0f;
-               // Debug.Log("Entered2");
-            } 
+                // Debug.Log("Entered2");
+            }
         }
-        
-        
+
+
         ////////////////////////////////////////////////////////
 
-        //playerVirtualCamera.m_XAxis.Value = Mathf.Clamp(playerVirtualCamera.m_XAxis.Value, 90, 270);
 
     }
-
-    /*
-    public float GetAxisCustom(string axisName)
-    {
-        Debug.Log("FART");
-        if (axisName == "Horizontal")
-        {
-            return -UnityEngine.Input.GetAxis("Horizontal");
-        }
-        else
-            return UnityEngine.Input.GetAxis(axisName);
-    }
-    */
-    /*
-    private float GetAxisCustom(string axisName)
-    {
-        Debug.Log("Hoi!");
-        if (axisName == "RightStickX")
-        {
-            Debug.Log("it has found right!");
-            if (playerVirtualCamera.m_XAxis.Value >= 90 && playerVirtualCamera.m_XAxis.Value <= 270)
-            {
-                Debug.Log("It was discoverd");
-                return 0;   
-            }
-            else
-                return Input.GetAxis("RightStickX");
-        }
-        else
-            return 0;
-    }
-
-    */
-
-    //Deffault Axis controls (X axis -> Input Axis Name = Mouse X, Y axis -> Input Axis Name = Mouse Y)
-
 }
