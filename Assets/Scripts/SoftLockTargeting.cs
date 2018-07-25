640b1e619e58724dff624ddb89249262082ef3e1
Dustin33 <38299144+Dustin33@users.noreply.github.com>
2018-07-25 17:00:06 -0400

    Revert "Dustin"

---
+++ Assets/Scripts/SoftLockTargeting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoftLockTargeting : MonoBehaviour {
+
+    private GameObject softLockTarget;
+
+    private float softLockMaxDistance;
+
+    //private GameObject player;
+
+
+    Ray softLockRaycast;
+
+
+    // Use this for initialization
+    void Start () {
+        softLockRaycast = new Ray(transform.position, Vector3.forward);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
